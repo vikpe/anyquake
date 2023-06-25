fb0344a55cd602a5cd670067e7d6dec6aff29973
Viktor Persson <viktor.persson@arcsin.se>
2023-06-25 14:34:11 +0200

    Add plugins.

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -9,7 +9,10 @@ fn greet(name: &str) -> String {
 
 fn main() {
     tauri::Builder::default()
-        .invoke_handler(tauri::generate_handler![greet])
+        .plugin(tauri_plugin_fs_extra::init())
+        .plugin(tauri_plugin_upload::init())
+        .plugin(tauri_plugin_window_state::Builder::default().build())
+        .invoke_handler(tauri::generate_handler![greet, download])
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
 }
