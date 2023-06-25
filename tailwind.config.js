eff67106c977fdc4ed60df4b3d34bedcbf9dd6f4
Viktor Persson <viktor.persson@arcsin.se>
2023-06-25 14:32:51 +0200

    Add tailwind css.

---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.vue"
+  ],
+  theme: {
+    container: {
+      center: true,
+      padding: {
+        DEFAULT: "1rem",
+      }
+    },
+    extend: {},
+  },
+  plugins: [],
+}
