cb0b4015d90247df5a927404d963597655383793
Viktor Persson <viktor.persson@arcsin.se>
2023-07-27 12:46:01 +0200

    Update download functions.

--- core/src/download.rs
+++ core/src/download.rs
@@ -10,15 +10,34 @@ pub async fn download_file(url: &str, file_path: &str) -> Result<()> {
     let client = reqwest::Client::new();
     let response = client.get(url).send().await?;
 
-    let mut dest = File::create(file_path)?;
+    let mut file = File::create(file_path)?;
     let content = response.bytes().await?;
-    dest.write_all(&content)?;
+    file.write_all(&content)?;
 
     Ok(())
 }
 
-pub async fn download_files_async(file_urls: Vec<(&str, &str)>) -> Result<()> {
+pub async fn download_files(file_urls: Vec<(&str, &str)>) -> Result<()> {
     let tasks = file_urls.into_iter().map(|(url, file_path)| download_file(url, file_path));
+    let results = join_all(tasks).await;
+
+    for result in results {
+        if let Err(e) = result {
+            eprintln!("Error while downloading file: {:?}", e);
+        }
+    }
+
+    Ok(())
+}
+
+pub async fn download_file_to_dir(url: &str, dir_path: &str) -> Result<()> {
+    let filename = url.split('/').last().unwrap();
+    let file_path = format!("{}/{}", dir_path, filename);
+    download_file(url, &file_path).await
+}
+
+pub async fn download_files_to_dir(file_urls: Vec<&str>, dir_path: &str) -> Result<()> {
+    let tasks = file_urls.into_iter().map(|url| download_file_to_dir(url, dir_path));
     let results = join_all(tasks).await;
 
     for result in results {
