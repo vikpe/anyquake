c599f2937da21c67b05ef12130eb80bc6d6d5dfe
Viktor Persson <viktor.persson@arcsin.se>
2023-08-01 18:14:41 +0200

    Refactor assets.

--- core/src/assets.rs
+++ core/src/assets.rs
@@ -19,16 +19,19 @@ impl RestrictedDir {
     pub fn validate_path(&self, path: &PathBuf) -> Result<()> {
         match path.starts_with(&self.path) {
             true => Ok(()),
-            false => Err(anyhow!("Path '{}' is not within the restricted dir", path.display()))
+            false => Err(anyhow!(
+                "Path '{}' is not within the restricted dir",
+                path.display()
+            )),
         }
     }
 
-    pub fn get_path_to(&self, path: &PathBuf) -> PathBuf {
+    pub fn get_abs_path_to(&self, path: &PathBuf) -> PathBuf {
         self.path.join(path)
     }
 
     pub fn has_file(&self, path: &PathBuf) -> bool {
-        self.get_path_to(path).is_file()
+        self.get_abs_path_to(path).is_file()
     }
 
     pub fn delete_file(&self, file_path: &PathBuf) -> Result<()> {
@@ -36,24 +39,28 @@ impl RestrictedDir {
             return Ok(());
         }
 
-        let path_rel = self.get_path_to(file_path);
-        self.validate_path(&path_rel)?;
+        let path_abs = self.get_abs_path_to(file_path);
+        self.validate_path(&path_abs)?;
 
-        fs::remove_file(path_rel)?;
+        fs::remove_file(path_abs)?;
         Ok(())
     }
 
     pub fn create_dir(&self, path: &PathBuf) -> Result<()> {
-        let path_rel = self.get_path_to(path);
-        self.validate_path(&path_rel)?;
+        let path_abs = self.get_abs_path_to(path);
+        self.validate_path(&path_abs)?;
 
-        fs_extra::create_dir_all(&path_rel)
+        fs_extra::create_dir_all(&path_abs)
     }
 
     pub async fn download(&self, url: &str, dest: &PathBuf) -> Result<()> {
-        let dest_rel = self.get_path_to(dest);
-        self.validate_path(&dest_rel)?;
+        let path_abs = self.get_abs_path_to(dest);
+        self.validate_path(&path_abs)?;
 
-        download(&url, &dest_rel).await
+        println!("Creating dir '{}'", path_abs.display());
+        self.create_dir(dest)?;
+        println!("Downloading '{}' to '{}'", url, path_abs.display());
+
+        download(&url, &path_abs).await
     }
 }
