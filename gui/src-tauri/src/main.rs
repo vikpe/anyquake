d7762ce7d7d8d6b13a4779ededf1e94f4ff86fba
Viktor Persson <viktor.persson@arcsin.se>
2023-08-02 11:52:21 +0200

    Refactor gui mockup.

--- gui/src-tauri/src/main.rs
+++ gui/src-tauri/src/main.rs
@@ -2,12 +2,12 @@
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 
 use std::path::PathBuf;
+
 use serde_json::{json, Value};
 
-use anyquake_core::qutil;
-use anyquake_core::modules::ModuleLike;
 use anyquake_core::modules::ezquake::EzQuake;
-
+use anyquake_core::modules::ModuleLike;
+use anyquake_core::qutil;
 
 #[tauri::command]
 fn get_quake_info() -> Value {
@@ -16,21 +16,20 @@ fn get_quake_info() -> Value {
 }
 
 #[tauri::command]
-fn install_ezquake() -> Value {
-    let ez = EzQuake{};
-    let res = match ez.uninstall() {
-        Ok(_) => "ok",
-        Err(_) => "not ok",
+fn uninstall_ezquake() -> Value {
+    let ez = EzQuake {};
+    let res: String = match ez.uninstall() {
+        Ok(_) => String::from("ok"),
+        Err(err) => String::from(err.to_string()),
     };
     return json!(res);
 }
 
 
-
 fn main() {
     tauri::Builder::default()
         .plugin(tauri_plugin_window_state::Builder::default().build())
-        .invoke_handler(tauri::generate_handler![get_quake_info, install_ezquake])
+        .invoke_handler(tauri::generate_handler![get_quake_info, uninstall_ezquake])
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
 }
--- gui/src/components/Greet.vue
+++ gui/src/components/Greet.vue
@@ -1,20 +1,9 @@
 <script lang="ts" setup>
-import { ref } from "vue";
-import { invoke } from "@tauri-apps/api/tauri";
-//import { download } from "tauri-plugin-upload-api";
-import { fs, os } from "@tauri-apps/api";
+import {ref} from "vue";
+import {invoke} from "@tauri-apps/api/tauri";
 import TaskIcon from "./TaskIcon.vue";
-import {
-  Task,
-  TaskOutcome,
-  TaskStatus,
-} from "./types";
+import {Task, TaskOutcome, TaskStatus,} from "./types";
 
-const downloadMsg = ref("click to download ezQuake");
-const progressRef = ref({
-  totalProgress: 0,
-  totalBytes: 0,
-});
 
 const infoTask = ref<Task>({
   status: TaskStatus.IDLE,
@@ -22,67 +11,55 @@ const infoTask = ref<Task>({
   data: [],
 });
 
-async function onGetInfoClick() {
+const installEzquakeTask = ref<Task>({
+  status: TaskStatus.IDLE,
+  outcome: TaskOutcome.UNDEFINED,
+  data: [],
+});
+
+async function onGetInfo() {
   infoTask.value.status = TaskStatus.IN_PROGRESS;
   const result: string[] = await invoke("get_quake_info");
   infoTask.value.data = result;
   infoTask.value.status = TaskStatus.COMPLETED;
   infoTask.value.outcome =
-    result.length > 0 ? TaskOutcome.SUCCESS : TaskOutcome.FAIL;
+      result.length > 0 ? TaskOutcome.SUCCESS : TaskOutcome.FAIL;
 }
 
-async function downloadEzQuake() {
-  const ezquakeUrl =
-    "https://github.com/QW-Group/ezquake-source/releases/download/3.6.2/ezquake.exe";
-  downloadMsg.value = await invoke("download", { url: ezquakeUrl });
-
-  const tmpDir = await os.tempdir();
-  const targetDirPath = `${tmpDir}/anyquake`;
-  const targetFilePath = `${targetDirPath}/ezquake.exe`;
-  await fs.createDir(targetDirPath, { recursive: true });
-  console.log("targetFilePath", targetFilePath);
-
-  // await download(
-  //     ezquakeUrl,
-  //     targetFilePath,
-  //     (progress: number, total: number) => {
-  //       console.log(`Downloaded ${progress} of ${total} bytes`);
-  //       progressRef.value = {
-  //         totalProgress: progressRef.value.totalProgress + progress,
-  //         totalBytes: total
-  //       }
-  //     }
-  // );
+async function onUnintallEzquake() {
+  installEzquakeTask.value.status = TaskStatus.IN_PROGRESS;
+  const result: string[] = await invoke("uninstall_ezquake");
+  installEzquakeTask.value.data = result;
+  installEzquakeTask.value.status = TaskStatus.COMPLETED;
+  installEzquakeTask.value.outcome =
+      result.length > 0 ? TaskOutcome.SUCCESS : TaskOutcome.FAIL;
 }
 
-function bytesInMb(bytes: number): string {
-  return `${Math.round(bytes / 1024 / 1024)} mb`;
-}
 </script>
 
 <template>
   <div>
-    <button class="bg-sky-600 text-white rounded p-3" @click="onGetInfoClick">
+    <button class="bg-sky-600 text-white rounded p-3" @click="onGetInfo">
       Find Quake Installations
     </button>
 
-    <hr class="my-2" />
+    <hr class="my-2"/>
 
-    <TaskIcon :task="infoTask" />
+    <TaskIcon :task="infoTask"/>
     <span class="font-bold">QuakeDirPath</span>:
     <span class="font-mono">{{ JSON.stringify(infoTask.data, null, 2) }}</span>
   </div>
 
-  <hr class="my-6" />
+  <hr class="my-6"/>
 
-  <button class="bg-blue-600 text-white p-3" @click="downloadEzQuake">
-    Download ezQuake
-  </button>
-  {{ downloadMsg }}
+  <div>
+    <button class="bg-sky-600 text-white rounded p-3" @click="onUnintallEzquake">
+      Uninstall ezQuake
+    </button>
 
-  <hr class="my-6" />
-  {{ infoTask }}
-  <hr class="my-6" />
-  {{ bytesInMb(progressRef.totalProgress) }} of
-  {{ bytesInMb(progressRef.totalBytes) }}
+    <hr class="my-2"/>
+
+    <TaskIcon :task="installEzquakeTask"/>
+    <span class="font-mono">{{ JSON.stringify(installEzquakeTask.data, null, 2) }}</span>
+  </div>
 </template>
